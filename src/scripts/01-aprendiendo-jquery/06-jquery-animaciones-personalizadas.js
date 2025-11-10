6dabcbb5e5d997a88dcc772d9108351b3b672836
Antonio <antonicut@gmail.com>
2025-11-10 22:06:03 +0100

    Refactor: Animaciones Personalizadas. Estilos CSS y Animaciones jQuery

--- src/scripts/01-aprendiendo-jquery/06-jquery-animaciones-personalizadas.js
+++ src/scripts/01-aprendiendo-jquery/06-jquery-animaciones-personalizadas.js
@@ -29,48 +29,89 @@
      * @type {JQuery<HTMLButtonElement>}
      */
     const $btnAnimame = $('#btnAnimame');
-              
-    
-    //  -----  Evento Click del Botón para iniciar la Animación  -----
+
+
     $btnAnimame.on('click', function () {
 
-        $caja
 
-            .animate({
-                marginLeft: '500px',
-                fontSize: '40px',
-                fontFamily: 'Tahoma',
-                height: '110px'
-            }, 5000)
+        /**
+         * - Ancho del viewport (ventana del navegador). 
+         * @type {number} */
+        const viewportWidth = $(window).width() ?? 0; // Si width() es undefined, usamos 0
 
+        if (viewportWidth < 768) {
             
-            .animate({
-                borderRadius: '900px',
-                lineHeight: '110px',
-                marginTop: '120px'
-            }, 'slow')
-           
-            .animate({
-                borderRadius: '0px',
-                marginLeft: '-420px'
-            }, 5000)
+            //  -----  Animación para móviles  -----
+            console.log('Animación para móviles');
             
-            .animate({
-                borderRadius: '0px',
-                marginTop: '20px',
-                height: '50px',
-                lineHeight: '50px',
-                fontFamily: 'Verdana',
-                fontSize: '19px'
-            }, 'slow',
+            $caja
                 
-                //  -----  callback de función  -----
-                function () {        
-                    setTimeout(() => alert('Animación Terminada!!!'), 1000);
-                })
-            
-    });
+                .animate({
+                    marginLeft: '15rem',
+                    fontSize: '1rem',
+                    height: '3.5rem'
+                }, 3000)
+                
+                .animate({
+                    borderRadius: '300px',
+                    height: '7rem',
+                    marginTop: '10rem',
+                    fontSize: '1.6rem'
+                }, 'slow')
+                
+                .animate({
+                    borderRadius: '0px',
+                    marginLeft: '1rem'
+                }, 3000)
+                
+                .animate({
+                    borderRadius: '0px',
+                    marginTop: '20px',
+                    height: '3.5rem',
+                    fontSize: '1rem'
+                }, 'slow', 
+                
+                    function () {
 
+                        setTimeout(() => alert('Animación terminada en Tablet y Móvil'), 500);
+                    });
+
+        } else {
+            
+            //  -----  Animación para escritorio  -----
+            console.log('Animación para escritorio');
+            
+            $caja
+                
+                .animate({
+                    marginLeft: '50rem',
+                    fontSize: '4rem',
+                    height: '11rem'
+                }, 5000)
+                
+                .animate({
+                    borderRadius: '900px',
+                    marginTop: '12rem',
+                }, 'slow')
+                
+                .animate({
+                    borderRadius: '0px',
+                    marginLeft: '5rem'
+                }, 5000)
+                
+                .animate({
+                    borderRadius: '0px',
+                    marginTop: '2rem',
+                    height: '5rem',
+                    fontSize: '1.6rem'
+                }, 'slow', 
+                
+                    function () {
+                        setTimeout(() => alert('Animación terminada en Escritorio'), 1000);
+                    });
+        }
+
+    });
 
 
 })(jQuery);
